ad0338a9499cf56c786134a9d2ae83c0ac3bda8d
Landon Whitesides <whitesideslandon@gmail.com>
2020-11-16 16:17:38 -0700

    created model and controller'

---
+++ db/migrate/20201116225418_create_notes.rb
@@ -0,0 +1,12 @@
+class CreateNotes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :notes do |t|
+      t.string :title
+      t.string :author
+      t.string :subject
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
