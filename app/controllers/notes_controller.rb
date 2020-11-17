c5076f2c5edc49a364c70755a59f1d6300e57c46
Landon Whitesides <whitesideslandon@gmail.com>
2020-11-16 17:25:07 -0700

    client can add new

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,7 @@
 class NotesController < ApplicationController
+
+  attr_accessor
+
   def index
     @notes = Note.all
   end
@@ -10,4 +13,21 @@ class NotesController < ApplicationController
   def new
     @note = Note.new
   end
+
+  def create
+    @note = Note.new(notes_params)
+
+    if @note.save
+      redirect_to notes_path
+    else
+      render :new
+    end
+  end
+
+  # private
+
+  def notes_params
+    params.require(:note).permit(:title, :author, :subject, :body)
+  end
+
 end
