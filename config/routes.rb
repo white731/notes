5ee0acd5f5401d07dde3f85053777ba63c90d503
Landon Whitesides <whitesideslandon@gmail.com>
2020-11-16 20:46:09 -0700

    updated home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
 
   root 'static_notes#home'
 
-  get '/about', to: 'static_pages#about'
-  
+
   resources :notes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
